412cd64c89e8f0d4323b20e1b6da2e37e0db6319
jephthah-orobia <jephthah.orobia@gmail.com>
2024-12-16 01:00:43 -0600

    Add clearing of console to all sample actions.

--- src/main.cpp
+++ src/main.cpp
@@ -12,40 +12,50 @@
 
 using namespace std;
 
-
-
 int main()
 {
-  
+
   char name[50];
 
-  const char* divider = "\n================================================\n";
+  const char *divider = "\n================================================\n";
 
-  cout << "Let's try basic input-output console routine!" << endl << divider << endl;
+  cout << "Let's try basic input-output console routine!" << endl
+       << divider << endl;
 
   cout << "What is your first name? ";
 
   cin >> name;
 
-  cout << "Hello " << name << endl << divider << endl;
+  cout << "Hello " << name << endl
+       << divider << endl;
 
   map<char, tuple<string, function<int()>>> actions = {
-    {'A', make_tuple("Re-Run App", main)},
-    {'B', make_tuple("Count 10 to 1 the re-run app",
-        [](){
-        for(int i = 10; i > 0; i--){
-          cout << i << endl;
-        };
-        return main();
-    })},
-    {'b', make_tuple("Count 1 to 10 the re-run app",
-      [](){
-        for(int i = 1; i <= 10; i++){
-          cout << i << endl;
-        };
-        return main();
-    })}
-  };
+      {'A', make_tuple("Re-Run App",
+                       []()
+                       {
+                         system("cls");
+                         return main();
+                       })},
+      {'B', make_tuple("Count 10 to 1 the re-run app",
+                       []()
+                       {
+                         system("cls");
+                         for (int i = 10; i > 0; i--)
+                         {
+                           cout << i << endl;
+                         };
+                         return main();
+                       })},
+      {'b', make_tuple("Count 1 to 10 the re-run app",
+                       []()
+                       {
+                         system("cls");
+                         for (int i = 1; i <= 10; i++)
+                         {
+                           cout << i << endl;
+                         };
+                         return main();
+                       })}};
 
   return whatsNext(actions);
 }
